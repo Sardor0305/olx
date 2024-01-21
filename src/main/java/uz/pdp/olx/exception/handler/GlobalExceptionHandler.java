5b031a22ebd5da2958b7b8d8bcf4598f5cfca9ed
escobar <erhardo2002@mail.ru>
2024-01-21 23:06:09 +0500

    OLX-5-mistake-exception bug fixed

---
+++ src/main/java/uz/pdp/olx/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package uz.pdp.olx.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import uz.pdp.olx.exception.TokenIsExpiredException;
+import uz.pdp.olx.exception.TokenNotFoundException;
+import uz.pdp.olx.exception.UserNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFondException(UserNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(TokenNotFoundException.class)
+    public ResponseEntity<?> handleTokenNotFoundException(TokenNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(TokenIsExpiredException.class)
+    public ResponseEntity<?> handleTokenIsExpiredException(TokenIsExpiredException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(exception.getMessage());
+    }
+
+
+
+}
--- src/main/java/uz/pdp/olx/service/AuthService.java
+++ src/main/java/uz/pdp/olx/service/AuthService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.pdp.olx.enitiy.Authentication;
 import uz.pdp.olx.enitiy.User;
+import uz.pdp.olx.exception.TokenIsExpiredException;
+import uz.pdp.olx.exception.TokenNotFoundException;
+import uz.pdp.olx.exception.UserNotFoundException;
 import uz.pdp.olx.jwt.JwtTokenProvider;
 import uz.pdp.olx.repository.AuthenticationRepository;
 import uz.pdp.olx.repository.UserRepository;
@@ -14,19 +17,18 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class AuthService {
     private final AuthenticationRepository authenticationRepository;
-    private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
     public void validateUserAuthentication(final String token) {
        final var authentication = authenticationRepository.findByToken(token).orElseThrow(
-                () -> new RuntimeException("Token not found")
+                () -> TokenNotFoundException.byToken(token)
         );
         final var userNotFound = authenticationRepository.findById(authentication.getUser().getId()).orElseThrow(
-                () -> new RuntimeException("user not found")
+                () -> UserNotFoundException.byId(authentication.getUser().getId())
         );
 
         if (authentication.getExpirationTime().isBefore(LocalDateTime.now())) {
-            throw new RuntimeException("Token already expired");
+            throw TokenIsExpiredException.expiryDate();
         }
 
         System.out.println(token);
