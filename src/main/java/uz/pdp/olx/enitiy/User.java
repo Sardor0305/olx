d69ebe7d48b79776aaadf8b46daacc46299bacca
Sardor0305 <87496899+Sardor0305@users.noreply.github.com>
2024-01-21 13:02:25 +0500

    Update User.java
    users turn to _user

--- src/main/java/uz/pdp/olx/enitiy/User.java
+++ src/main/java/uz/pdp/olx/enitiy/User.java
@@ -24,7 +24,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
